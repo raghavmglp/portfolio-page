3beab02ea226ce1a6a5375d683e4c160df33ee7b
raghavmglp <raghavmtests@gmail.com>
2025-12-31 19:54:12 +0100

    padding fix footer, removing highlight for now

--- portfolio-page/src/pages/body/home.tsx
+++ portfolio-page/src/pages/body/home.tsx
@@ -3,12 +3,13 @@ import { Text } from "@/components/ui/typography";
 import { contactInfo } from "@/constants/contact";
 
 const PASTEL_COLORS = [
-  // Matches bg-pink-100
-  "bg-pink-100 dark:bg-rose-900/100",
-  // Matches bg-blue-100
-  "bg-orange-100 dark:bg-amber-900/100",
-  // Matches bg-green-100
-  "bg-green-100 dark:bg-emerald-900/100",
+  "dark:text-rose-100 text-rose-900", // Soft Pink
+  "dark:text-sky-100 text-sky-900", // Airy Blue
+  "dark:text-emerald-100 text-emerald-900", // Fresh Green
+  "dark:text-violet-100 text-violet-900", // Calm Purple
+  "dark:text-amber-100 text-amber-900", // Warm Gold/Orange
+  "dark:text-teal-100 text-teal-900", // Ocean Blue/Green
+  "dark:text-fuchsia-100 text-fuchsia-900", // Vibrant Pink/Purple
 ];
 
 // 2. The Random Highlighter
@@ -34,32 +35,30 @@ const Highlight = ({ children }: { children: React.ReactNode }) => {
 
 function Home() {
   return (
-    <div className="space-y-4 leading-relaxed">
-      <Text className="text-[18px]">
-        Hi! I'm <Highlight>Raghav</Highlight>.
-      </Text>
+    <div className="flex flex-col min-h-[80vh] space-y-4 leading-relaxed">
+      {/* Content wrapper */}
+      <div className="space-y-4">
+        <Text className="text-[18px]">Hi! I'm Raghav.</Text>
 
-      <Text className="text-[18px]">
-        I'm currently pursuing a{" "}
-        <Highlight>Master's Degree in Computer Science</Highlight> at the{" "}
-        <Highlight>Technical University of Eindhoven</Highlight>. I'm interested
-        in the intersection of <Highlight>systems architecture</Highlight> and{" "}
-        <Highlight>machine learning</Highlight>. I have 2 years of professional
-        experience in software engineering across a variety of technologies. My
-        undergraduate studies were at the <Highlight>Birla Institute of Technology and Science (BITS), Pilani</Highlight>
+        <Text className="text-[18px]">
+          I'm currently pursuing a Master's Degree in Computer Science at the
+          Technical University of Eindhoven. I'm interested in the intersection
+          of systems architecture and machine learning. I have 2 years of
+          professional experience in software engineering across a variety of
+          technologies. My undergraduate studies were at the Birla Institute of
+          Technology and Science (BITS), Pilani.
+        </Text>
 
-        .
-      </Text>
+        <Text className="text-[18px]">
+          You can view some of my personal projects below. I also write from
+          time to time: if you're interested in reading some of my work, check
+          out the blog! I'm always looking for new opportunites! If you'd like
+          to get in touch with me, you can find my contact information at the
+          bottom of the page.
+        </Text>
+      </div>
 
-      <Text className="text-[18px]">
-        You can view some of my personal projects below. I also write from time
-        to time: if you're interested in reading some of my work, check out the
-        blog! I'm always looking for new opportunites! If you'd like to get in
-        touch with me, you can find my contact information at the bottom of the
-        page.
-      </Text>
-
-      <div className="flex absolute bottom-4 left-0 right-0 justify-center items-center mt-8">
+      <div className="flex justify-center items-center mt-4 pt-12 pb-4">
         <div className="flex gap-4">
           {contactInfo.map((contact) => (
             <a
