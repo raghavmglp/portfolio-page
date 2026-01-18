864e65aacb1196bfb27cc7a95ab7a271dc299aa3
raghavmglp <raghavmtests@gmail.com>
2026-01-19 00:56:07 +0100

    misc home changes

--- portfolio-page/src/pages/body/home.tsx
+++ portfolio-page/src/pages/body/home.tsx
@@ -4,36 +4,36 @@ import { contactInfo } from "@/constants/contact";
 function Home() {
   return (
     <div className="flex flex-col min-h-[90vh] leading-relaxed max-w-5xl mx-auto overflow-visible">
-        <div className="flex flex-col md:flex-row gap-10 items-center md:items-start overflow-visible">
-          <div className="w-full md:w-52 shrink-0 flex justify-center md:block overflow-visible">
-            <DraggablePhoto />
-          </div>
+      <div className="flex flex-col md:flex-row gap-10 items-center md:items-start overflow-visible">
+        <div className="w-full md:w-52 shrink-0 flex justify-center md:block overflow-visible">
+          <DraggablePhoto />
+        </div>
 
-          <div className="flex-1 space-y-6 w-full text-center md:text-left">
-            <p className="text-[20px] font-medium text-foreground tracking-tight">
-              Hi! I'm Raghav.
+        <div className="flex-1 space-y-6 w-full text-center md:text-left">
+          <p className="text-[20px] font-medium text-foreground tracking-tight">
+            Hi! I'm Raghav.
+          </p>
+
+          <div className="space-y-5">
+            <p className="text-[18px] text-foreground/90">
+              I'm currently pursuing a Master's Degree in Computer Science at
+              the Technical University of Eindhoven. I'm interested in the
+              intersection of systems architecture and machine learning. I have
+              2 years of professional experience - I was a software engineer at
+              Eltropy and an intern at Amazon. My undergraduate studies were at
+              the Birla Institute of Technology and Science (BITS), Pilani.
             </p>
 
-            <div className="space-y-5">
-              <p className="text-[18px] text-foreground/90">
-                I'm currently pursuing a Master's Degree in Computer Science at
-                the Technical University of Eindhoven. I'm interested in the
-                intersection of systems architecture and machine learning. I have
-                2 years of professional experience - I was a software engineer at
-                Eltropy and an intern at Amazon. My undergraduate studies were at
-                the Birla Institute of Technology and Science (BITS), Pilani.
-              </p>
-
-              <p className="text-[18px] text-foreground/90">
-                You can view some of my personal projects below. I also write from
-                time to time - if you're interested in reading some of my work,
-                check out the blog! I'm always looking for new opportunities. If
-                you'd like to get in touch with me, you can find my contact
-                information at the bottom of the page.
-              </p>
-            </div>
+            <p className="text-[18px] text-foreground/90">
+              You can view some of my personal projects in the projects section.
+              I also write from time to time - if you're interested in reading
+              some of my work, check out the blog! I'm always looking for new
+              opportunities. If you'd like to get in touch with me, you can find
+              my contact information at the bottom of the page.
+            </p>
           </div>
         </div>
+      </div>
 
       {/* FOOTER SECTION */}
       <div className="flex justify-center items-center mt-auto pt-24 pb-12">
